f1c987b92892cde87599b9f207df9759e4397203
Julio Borges <mail>
2020-05-10 17:11:34 +0200

    Refactoring

--- Portfolio.py
+++ Portfolio.py
@@ -7,7 +7,14 @@ import math
 
 
 class Portfolio():
-    def __init__(self, tickers: list, weights: list, start_investment: float, start=None, end=None, period = "10y"):
+    def __init__(
+            self,
+            tickers: list,
+            weights: list,
+            start_investment: float,
+            start=None,
+            end=None,
+            period="10y"):
         assert(len(tickers) == len(weights))
         assert(np.sum(weights) == 1)
         self.assets = []
@@ -26,16 +33,17 @@ class Portfolio():
         self._calibrate_dates()
         self._calculate_values()
 
-
     def _calibrate_dates(self):
-        common_dates = set.intersection(*[set(asset.index.unique()) for asset in self.assets])
-        self.assets = [asset[asset.index.map(lambda x: x in common_dates)] for asset in self.assets]
+        common_dates = set.intersection(
+            *[set(asset.index.unique()) for asset in self.assets])
+        self.assets = [asset[asset.index.map(
+            lambda x: x in common_dates)] for asset in self.assets]
 
-
-    def _calculate_asset_development(self, idx, weighted = True):
+    def _calculate_asset_development(self, idx, weighted=True):
         values = []
         if weighted:
-            stocks = self.start_investment * self.weights[idx] / self.assets[idx]["Close"].iloc[0]
+            stocks = self.start_investment * \
+                self.weights[idx] / self.assets[idx]["Close"].iloc[0]
         else:
             stocks = self.start_investment / self.assets[idx]["Close"].iloc[0]
         for j, row in self.assets[idx].iterrows():
@@ -45,11 +53,10 @@ class Portfolio():
                 stocks = stocks + (dividends / row["Close"])
                 try:
                     self.dividends[j] += dividends
-                except:
+                except BaseException:
                     self.dividends[j] = dividends
             values.append(value)
         return pd.Series(values, index=self.assets[idx].index)
-
 
     def _calculate_values(self):
         all_values = []
@@ -60,17 +67,18 @@ class Portfolio():
         self.hist = pd.Series(valuesmat, index=ss.index)
         self.values = all_values
 
-
     def plot(self, hline=True):
         """
         Plot portfolio development
         :param hline: Show horizontal line with initial investment
         """
-        self._set_heat_params()
-        self.hist.plot(label = "Portfolio", title = "Portfolio Development")
-        plt.hlines(self.start_investment, self.hist.index.min(), self.hist.index.max(), colors="r")
+        self.hist.plot(label="Portfolio", title="Portfolio Development")
+        plt.hlines(
+            self.start_investment,
+            self.hist.index.min(),
+            self.hist.index.max(),
+            colors="r")
         plt.grid()
-
 
     def plot_comparison(self):
         """
@@ -78,11 +86,13 @@ class Portfolio():
         """
         for i in range(self.n):
             ss = self._calculate_asset_development(i, False)
-            ss.plot(label = self.tickers[i])
-        self.hist.plot(label="Portfolio", title = "Simulation of Portfolio vs. 100%% Initial Investment on individual stocks\n Simulated Investment = %.2f" % self.start_investment)
+            ss.plot(label=self.tickers[i])
+        self.hist.plot(
+            label="Portfolio",
+            title="Simulation of Portfolio vs. 100%% Initial Investment on individual stocks\n Simulated Investment = %.2f" %
+            self.start_investment)
         plt.grid()
         plt.legend()
-
 
     def summary(self):
         """
@@ -90,15 +100,16 @@ class Portfolio():
         """
         win = self.hist.iloc[-1] - self.hist.iloc[0]
         years = (self.hist.index[-1] - self.hist.index[0]).days / 365
-        yy = math.pow(self.hist.iloc[-1]/self.hist.iloc[0] , 1/years) -1
+        yy = math.pow(self.hist.iloc[-1] / self.hist.iloc[0], 1 / years) - 1
         dividends = pd.Series(self.dividends).sum()
 
-        t2 = "Capital Development: from %.2f to %.2f" % (self.hist.iloc[0], self.hist.iloc[-1]) + "\n"
+        t2 = "Capital Development: from %.2f to %.2f" % (
+            self.hist.iloc[0], self.hist.iloc[-1]) + "\n"
         t3 = "Yield: %.4f" % yy + "\n"
-        t4 = "Gain: %.4f; From which Dividends: %.4f; and Asset Price Development: %.4f" % (win, dividends, win-dividends)
+        t4 = "Gain: %.4f; From which Dividends: %.4f; and Asset Price Development: %.4f" % (
+            win, dividends, win - dividends)
 
         print(t2, t3, t4)
-
 
     def generate_saving_plan(self, monthly_investment):
         sparlist = []
@@ -111,8 +122,8 @@ class Portfolio():
         valuesmat = np.array(sparlist).sum(axis=0)
         self.sparhist = pd.Series(valuesmat, index=ss.index)
         mindex = ss.groupby(pd.Grouper(freq='M')).mean().index
-        self.spar_dev = pd.Series([monthly_investment*i for i in range(1, len(mindex-1)+1)], index=mindex)
-
+        self.spar_dev = pd.Series(
+            [monthly_investment * i for i in range(1, len(mindex - 1) + 1)], index=mindex)
 
     def _calculate_sparplan(self, df_asset, investment):
         lastmonth = df_asset.index[0].month
--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 **Portfolio historical development simulation:** Given a portfolio with stocks - every stock assigned a given weight - what would be the portfolio development for a given period?
 
-![Example](example.jpeg)
+![Example](example.png)
